24c74a0e24cca7cc5c9cb586b7a321501376a201
Praveen Bandla <praveenbandla@10-17-38-17.dynapool.wireless.nyu.edu>
2025-03-13 19:36:34 -0400

    Finishing documentation for now

---
+++ src/utils/data_handler.py
@@ -0,0 +1,3 @@
+'''
+This file will contain utlity functions to handle data. It will include methods to read and write files.
+'''
---
+++ src/utils/prompt.py
@@ -0,0 +1,3 @@
+'''
+This class will be used to manage Prompts. It will include methods to create prompt, access prompts, write prompts, read prompts.
+'''
