495a08d84236981440d30fd2dfdb6220fad9e3f9
Praveen Bandla <praveenbandla@10-19-155-47.dynapool.wireless.nyu.edu>
2025-03-18 15:14:37 -0400

    adding changes

---
+++ configs/data_size_configs.py
@@ -0,0 +1,8 @@
+'''
+This script contains all information related to the data size configurations. I have added some example things but this is not complete. This is just a starting point. Add to it as needed.
+'''
+
+NUM_PROMPT_VARIATIONS = 0
+NUM_BASE_PROMPTS = 0
+PARTITION_SIZE_MODEL_OUTPUT_PARQUET = 0
+
