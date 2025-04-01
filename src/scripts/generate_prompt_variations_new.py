bb14037ebb914ceaf2d40e8981ffa1fa1bca3d51
rebrinehart <135187192+rebrinehart@users.noreply.github.com>
2025-04-01 16:25:56 -0400

    replace with correct script

--- src/scripts/generate_prompt_variations_new.py
+++ src/scripts/generate_prompt_variations_new.py
@@ -1,27 +1,26 @@
 '''
-Generate Base Prompts (New)
+Generate Prompt Varitions (New)
 
-This updated script generates a set of base prompts by calling the main model to produce base prompts based on the stored inputs. It runs inferences and generates 'n' base prompts for further use (n found in the data_size_configs file).
+This updated script generates a set of prompt variations by calling the prompt variation model to produce prompt variations based on the stored base prompts. It runs inferences and generates 'NUM_PROMPT_VARIATIONS' prompt variations for further use.
 
 Inputs:
-    None
+    bp_idx: The index of the base prompt to generate prompt variations for.
 
 Dependencies:
-    - base_prompt_model_input.json: A JSON file containing instructions for generating base prompts.
-    - base_prompt_model_config.yaml: A YAML file specifying model parameters and settings.
-    - data_handler.py: base prompt class
-    - prompt.py: base prompt class
+    - prompt_variation_model_input.json: A JSON file containing instructions for generating base prompts.
+    - prompt_variation_model_config.yaml: A YAML file specifying model parameters and settings.
+    - data_handler.py: prompt variation class
+    - prompt.py: prompt variation class
 
 Outputs:
     None
 
 Process:
-1. Collect the instruction for generating base prompts. Uses the number_of_base_prompts from the data_size_configs file for n and the base_prompt_model_input.json file for the instruction.
+1. Collect the instruction for generating prompt variations. Uses the NUM_PROMPT_VARIATIONS from the data_size_configs file and the prompt_variation_model_input.json file for the instruction.
 2. Loads the model
 3. Loads the model configuration file
-4. Runs inference to collect all the base prompts
-5. Writes the base prompts to a SQLite database
-
+4. Runs inference to collect all the prompt variations
+5. Writes the prompt variations to a single parquet associated to the given base prompt
 '''
 
 from src.utils.prompt import *
@@ -34,27 +33,31 @@ import torch
 import transformers
 from transformers import AutoModelForCausalLM, AutoTokenizer, pipeline
 
-# Step 1: Collect the instruction for generating base prompts
+# Step 1: Collect the instruction for generating prompt variations
 def collect_instruction():
     '''
-    Creates instructions for generating base prompts. Uses the base_prompt_model_input.json file as the model input, along with NUM_BASE_PROMPTS from the data_size_configs file.
+    Creates instructions for generating prompt variations. Uses the prompt_variation_model_input.json file as the model input, along with NUM_PROMPT_VARIATIONS from the data_size_configs file.
 
     Inputs: None
 
     Outputs:
-        - full_prompt: A list containing the system role and content template for generating base prompts, stored as two dictionaries.
+        - full_prompt: A list containing the system role and content template for generating prompt variations, stored as two dictionaries.
     
     '''
-    if not os.path.exists(BASE_PROMPT_MODEL_INPUT):
-        raise FileNotFoundError(f"Instruction file not found at {BASE_PROMPT_MODEL_INPUT}")
-    with open(BASE_PROMPT_MODEL_INPUT, 'r') as f:
+    if not os.path.exists(PROMPT_VARIATION_MODEL_INPUT):
+        raise FileNotFoundError(f"Instruction file not found at {PROMPT_VARIATION_MODEL_INPUT}")
+    with open(PROMPT_VARIATION_MODEL_INPUT, 'r') as f:
         prompt_structure = json.load(f)
     
     system_role = prompt_structure["system_role"]
     content_template = prompt_structure["content_template"]
+
+    # Replace placeholders with actual values
+    content_template = content_template.replace("{bp_str}", #BP_STR)
+    content_template = content_template.replace("{num_prompt_variations}", str(NUM_PROMPT_VARIATIONS / 2))
     #instruction = system_role + " " + content_template
 
-    content = content_template.format(num_prompt = NUM_BASE_PROMPTS)
+    content = content_template.format(num_prompt = NUM_PROMPT_VARIATIONS)
 
     full_prompt = [
         {
@@ -79,9 +82,9 @@ def load_configs():
     Inputs: None
 
     Outputs: 
-        - configs: the configurations for the base prompt model.
+        - configs: the configurations for the prompt variation model.
     '''
-    config_path = BASE_PROMPT_MODEL_CONFIG
+    config_path = PROMPT_VARIATION_MODEL_CONFIG
 
     with open(config_path, 'r') as f:
         configs = yaml.safe_load(f)
@@ -90,17 +93,17 @@ def load_configs():
 
 def load_model():
     '''
-    Loads the base prompt model for inference. 
+    Loads the prompt variation model for inference. 
     '''
     model = AutoModelForCausalLM.from_pretrained(
-        BASE_PROMPT_MODEL,
+        PROMPT_VARIATION_MODEL,
         torch_dtype=torch.bfloat16,
         device_map="auto",
         trust_remote_code=True,
         local_files_only=True
     )
 
-    tokenizer = AutoTokenizer.from_pretrained(BASE_PROMPT_MODEL, trust_remote_code=True)
+    tokenizer = AutoTokenizer.from_pretrained(PROMPT_VARIATION_MODEL, trust_remote_code=True)
 
     pipe = pipeline(
         'text-generation', 
@@ -111,108 +114,49 @@ def load_model():
 
     return pipe
 
-    # model_id = "meta-llama/Llama-3.1-8B"
-    # tokenizer = AutoTokenizer.from_pretrained(model_id)
-    # tokenizer.chat_template = "<s>[INST] {instruction} [/INST] {response}</s>"  # Example template
-
-    # pipeline = transformers.pipeline(
-    #     "text-generation",
-    #     model=model_id,
-    #     tokenizer=tokenizer,
-    #     model_kwargs={"torch_dtype": torch.bfloat16},
-    #     device_map="auto",
-    # )
-
-    # return pipeline
-    
-
-# Step 2: Run inference to collect all the base prompts
-
-def base_prompt_inference(): 
+# Step 2: Run inference to collect all the prompt variations
+def prompt_variation_inference():
     '''
-    This runs inference on the base_prompt_model to generate the desired output. It solely retrieves the response as a string and does not process it further.
-    '''# current understanding: this will generate the x (i.e. 100) base prompts needed
+    Runs inference on the prompt_variation_model to generate the desired output. It solely retrievers the response as a string and does not process it further.
+    '''
     instruction = collect_instruction()
     pipe = load_model()
     configs = load_configs()
 
-    max_new_tokens = configs.get("max_new_tokens")
+    max_tokens = configs.get("max_tokens")
     temperature = configs.get("temperature")
     top_p = configs.get("top_p")
-    do_sample = configs.get("do_sample")
+    do_sample = configs.get("do_sample") # check this exists
 
     generation_args = {
-        "max_new_tokens": max_new_tokens,
+        "max_tokens": max_tokens,
         "temperature": temperature,
         "top_p": top_p,
         "do_sample": do_sample
     }
 
     print("Successfully loaded model and configs.")
-    print("Running inference to generate base prompts...")
+    print("Running inference to generate prompt variations...")
     #outputs = pipe(instruction, **generation_args)
     outputs = pipe(instruction)
     print("Inference complete.")
     print("Outputs: ", outputs[0]["generated_text"])
     return outputs[0]["generated_text"]
 
-def parse_model_output_as_bp_objects(model_output):
+def parse_model_output_as_pv_objects(model_output):
     '''
-    This function parses the model output to extract the base prompts as tuples of (bp_idx, base_prompt_string). NB: This assumes that the db is empty. This handles the randomization of ordering of prompts as well.
+    This function parses the model output to extract the prompt variations as tuples of (bpv_idx, prompt_variation_string).
 
     Inputs:
-        - model_output: The model output string from base_prompt_inference()
+        - model_output: The model output string from prompt_variation_inference()
 
     Outputs:
-        - list: A list of tuples, each containing the base prompt index and the base prompt string. Stored as a list of (bp_idx, bp_str)
+        - list: A list of tuples, each containing the base prompt variation index and the prompt variation string. Stored as a list of (bpv_idx, bpv_str)
     '''
     base_prompts = json.loads(model_output)
 
-    # creating random order of prompts stored as int indices
-    random_indices = random.sample(range(NUM_BASE_PROMPTS), NUM_BASE_PROMPTS)
+    # # creating random order of prompts stored as int indices
+    # random_indices = random.sample(range(NUM_BASE_PROMPTS), NUM_BASE_PROMPTS)
 
     # returning a list of tuples in the desired format of the random order of prompts
     return [(new_idx, base_prompts[random_idx]) for new_idx, random_idx in enumerate(random_indices)]
-
-
-# Step 3: Write the base prompts to a SQLite database
-def write_to_db(formatted_base_prompts, bp_db):
-    '''
-    This function writes the base prompts to the SQLite database using the BasePromptDB object.
-
-    Inputs:
-        - formatted_base_prompts: A list of tuples, each containing the base prompt index and the base prompt string. Stored as a list of (bp_idx, bp_str)
-        - bp_db: an object of type BasePromptDB (data handler) used to write
-    '''
-    bp_db.insert_base_prompts(formatted_base_prompts)
-
-    return
-
-
-def main():
-    if os.path.exists(SQL_DB): #ensures that we do not have a pre-existing database
-        os.remove(SQL_DB)
-    # creates the BasePromptDB object
-    bp_db = BasePromptDB()
-
-    # # # generates model output by inferencing
-    # model_output = base_prompt_inference()
-    
-    # # # formats the model output as a list of tuples in random order
-    # formatted_base_prompts = parse_model_output_as_bp_objects(model_output)
-
-    formatted_base_prompts = [(1, 'testing1'), (2, 'testing2'), (3, 'tesingt3'), (4, 'test4'), (5, 'test5')]
-    
-    # writes the base prompts to the SQLite database
-    write_to_db(formatted_base_prompts, bp_db)
-    # closes the connection to the database
-    bp_db.close_connection()
-    print("Base prompts generated and written to SQLite database.")
-
-
-if __name__ == "__main__":
-    main()
-    # pass
-    # bp_db = BasePromptDB()
-    # bp_db.reset_database()
-    
