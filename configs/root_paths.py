69303d730d589f75b4cdec9e72d3d3af8123c78a
rebrinehart <135187192+rebrinehart@users.noreply.github.com>
2025-04-10 00:36:55 -0400

    Add validator model ids

--- configs/root_paths.py
+++ configs/root_paths.py
@@ -17,7 +17,15 @@ MODEL_CONFIGS = os.path.join(CONFIGS_PATH, 'model_configs')
 
 # Validation model related files
 VALIDATION_SCORES = os.path.join(DATA_PATH, 'validation_scores')
+VALIDATOR_MODEL_INPUT = os.path.join(MODEL_INPUT_PATH, 'validator_model_input.json')
 VALIDATOR_MODEL_CONFIGS = os.path.join(CONFIGS_PATH, 'validator_model_config.yaml')
+FALCON_MAMBA_MODEL = os.path.join(MODELS_PATH, 'tiiuae/falcon-mamba-7b')
+FALCON_MAMBA_MODEL_ID = 'tiiuae/falcon-mamba-7b'
+OPT_MODEL = os.path.join(MODELS_PATH, 'facebook/opt-6.7b')
+OPT_MODEL_ID = 'facebook/opt-6.7b'
+MISTRAL_INSTRUCT_MODEL = os.path.join(MODELS_PATH, 'mistralai/Mistral-7B-Instruct-v0.2')
+MISTRAL_INSTRUCT_MODEL_ID = 'mistralai/Mistral-7B-Instruct-v0.2'
+RUBRIC_PATH = os.path.join(CONFIGS_PATH, 'rubric.txt')
 
 # BP and BPV related files
 SQL_DB = os.path.join(DATA_PATH, 'base_prompts', 'base_prompts.db')
@@ -40,6 +48,3 @@ MAIN_MODEL_INPUT = os.path.join(MODEL_INPUT_PATH, 'main_model_input.json')
 MAIN_MODEL_CONFIGS = os.path.join(MODEL_CONFIGS, 'main_model_config.yaml')
 MAIN_MODEL = os.path.join(MODELS_PATH, 'microsoft-Phi-3.5-mini-instruct/')   
 MAIN_MODEL_ID = "microsoft/Phi-3.5-mini-instruct"
-
-# redirect huggingface cache
-RR_HUGGINGFACE_CACHE = '/scratch/rrr9340/huggingface_cache'
