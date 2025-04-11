e8e53aba9e51f34bac11c80311de7d68c34b5046
Rebecca Rinehart <rrr9340@nyu.edu>
2025-04-11 02:08:07 +0000

    Change configs

--- configs/data_size_configs.py
+++ configs/data_size_configs.py
@@ -2,7 +2,7 @@
 This script contains all information related to the data size configurations. I have added some example things but this is not complete. This is just a starting point. Add to it as needed.
 '''
 
-NUM_PROMPT_VARIATIONS = 5
+NUM_PROMPT_VARIATIONS = 10
 NUM_BASE_PROMPTS = 10
 # PARTITION_SIZE_MODEL_OUTPUT_PARQUET = 0
 NUM_RUBRIC_SECTIONS = 5
