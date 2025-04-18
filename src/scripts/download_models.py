335d04668d4ce8d501833fd28220727577b4ceda
rebrinehart <135187192+rebrinehart@users.noreply.github.com>
2025-04-17 23:31:57 -0400

    Specify model cache directory

--- src/scripts/download_models.py
+++ src/scripts/download_models.py
@@ -37,13 +37,15 @@ prompt_opt_tokenizer = AutoTokenizer.from_pretrained(PROMPT_OPT_BASE_MODEL_ID)
 regression_head_base = AutoModelForCausalLM.from_pretrained(
     PROMPT_OPT_BASE_MODEL_ID,
     torch_dtype = torch.float16,
-    device_map="auto"
+    device_map="auto",
+    cache_dir = PROMPT_OPT_BASE_PATH
 )
 regression_head_model = PeftModel.from_pretrained(
     regression_head_base,
     LORA_REGRESSION_HEAD_PATH,
     torch_dtype=torch.float16,
-    device_map="auto"
+    device_map="auto",
+    cache_dir = LORA_REGRESSION_HEAD_PATH
 )
 print("Regression head base and model downloaded and saved locally.")
 
@@ -51,13 +53,15 @@ print("Regression head base and model downloaded and saved locally.")
 prompt_gen_base = AutoModelForCausalLM.from_pretrained(
     PROMPT_OPT_BASE_MODEL_ID,
     torch_dtype = torch.float16,
-    device_map="auto"
+    device_map="auto",
+    cache_dir = PROMPT_OPT_BASE_PATH
 )
 prompt_gen_model = PeftModel.from_pretrained(
     prompt_gen_base,
     LORA_PROMPT_GEN_PATH,
     torch_dtype=torch.float16,
-    device_map="auto"
+    device_map="auto",
+    cache_dir=LORA_PROMPT_GEN_PATH
 )
 print("Regression head base and model downloaded and saved locally.")
 # NOTE: Using device_map="auto" will automatically place the model on the same GPU if memory allows. Must ensure Greene job requests enough memory (24GB+)
